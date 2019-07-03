5ec60f704359ad47ccebed15a0b7ea452513a62a
justinm-nyc <justin.martinelli@oracle.com>
2019-07-03 13:49:12 -0400

    Added v-model and search object

--- directory.js
+++ directory.js
@@ -4,25 +4,30 @@
   Vue.component('directory-filters', {
     data: function(){
       return {
-        titles: window.LMDirectory.titles
+        search: {
+          name: '',
+          title: '',
+          intern: false
+        }
       }
     },
+    titles: window.LMDirectory.titles,
     template: '<form action="" id="directory-filters"> \
     <div class="group"> \
       <label for="txt-name">Name:</label> \
-      <input type="text" name="name" value="" placeholder="Name of employee" id="txt-name"> \
+      <input type="text" name="name" value="" placeholder="Name of employee" id="txt-name" v-model.trim="search.name"> \
     </div> \
     <div class="group"> \
       <label for="sel-title">Job Title:</label> \
-      <select name="sel-title" id="sel-title"> \
+      <select name="sel-title" id="sel-title" v-model="search.title"> \
         <option value="">- Select -</option> \
-        <option v-for="title in titles" :value="title.key"> \
+        <option v-for="title in $options.titles" :value="title.key"> \
         {{ title.display }} \
         </option> \
       </select> \
     </div> \
     <div class="group"> \
-      <label><input type="checkbox" value="1"> Intern</label> \
+      <label><input type="checkbox" value="1" v-model="search.intern"> Intern</label> \
     </div> \
   </form>'
   });
