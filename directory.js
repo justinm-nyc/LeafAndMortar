e894a991c9f341f259ec7b681963ce0c18087533
justinm-nyc <justin.martinelli@oracle.com>
2019-07-03 13:41:53 -0400

    Fix job title part

--- directory.js
+++ directory.js
@@ -2,11 +2,42 @@
   "use strict";
 
   Vue.component('directory-filters', {
-    template: '<form action="" id="directory-filters"><p>Insert filters here</p></form>'
+    data: function(){
+      return {
+        titles: window.LMDirectory.titles
+      }
+    },
+    template: '<form action="" id="directory-filters"> \
+    <div class="group"> \
+      <label for="txt-name">Name:</label> \
+      <input type="text" name="name" value="" placeholder="Name of employee" id="txt-name"> \
+    </div> \
+    <div class="group"> \
+      <label for="sel-title">Job Title:</label> \
+      <select name="sel-title" id="sel-title"> \
+        <option value="">- Select -</option> \
+        <option v-for="title in titles" :value="title.key"> \
+        {{ title.display }} \
+        </option> \
+      </select> \
+    </div> \
+    <div class="group"> \
+      <label><input type="checkbox" value="1"> Intern</label> \
+    </div> \
+  </form>'
   });
 
   Vue.component('directory-people', {
-    template: '<p>Insert people here</p>'
+    props: ["people"],
+    template: '<div class="results"> \
+    <div class="person" v-for="person in people"> \
+    <h3> {{ person.name }}, {{ person.title }}</h3> \
+    <p> \
+      <img class="size-medium wp-image-14 alignright" :src="person.img" :alt="person.name" width="300" height="300"  sizes="(max-width: 300px) 100vw, 300px" /> \
+      {{ person.bio }} \
+    </p> \
+    </div> \
+    </div>'
   });
 
   var Directory = new Vue({
